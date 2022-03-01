a1f120b3e01d5814e12550ea0edb736f7645bb05
GnanaDivya <gnanadivyabhimanadula@gmail.com>
2022-03-01 17:07:32 +0000

    sample

---
+++ jspservlet-app-mymusic/src/main/java/model/Song.java
@@ -0,0 +1,23 @@
+package model;
+import java.lang.*;
+import java.util.*;
+public class Song{
+
+    public static void main(String args[]){
+        int songId;
+        String songName,singerName,imageUrl,songUrl;
+
+    }
+    public void addSong(String Song){
+
+    }
+    public void viewSongById(){
+
+    }
+    public void updateSong(string Song){
+
+    }
+    public void deleteSong(int songId){
+
+    }
+}
